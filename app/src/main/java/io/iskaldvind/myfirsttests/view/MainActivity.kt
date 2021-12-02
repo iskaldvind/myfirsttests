a30facdae1c35fa1cf8cd6ca70f62e328d42743a
iskaldvind <iskaldvind@gmail.com>
2021-12-02 18:18:30 +0300

    hw

---
+++ app/src/main/java/io/iskaldvind/myfirsttests/view/MainActivity.kt
@@ -0,0 +1,34 @@
+package io.iskaldvind.myfirsttests.view
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import io.iskaldvind.myfirsttests.R
+import io.iskaldvind.myfirsttests.databinding.ActivityMainBinding
+import io.iskaldvind.myfirsttests.utils.EmailValidator
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val emailValidator = EmailValidator()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            emailInput.addTextChangedListener(emailValidator)
+
+            saveButton.setOnClickListener {
+                if (emailValidator.isValid) {
+                    Toast.makeText(this@MainActivity, getString(R.string.valid_email), Toast.LENGTH_SHORT).show()
+                } else {
+                    val errorEmail = getString(R.string.invalid_email)
+                    emailInput.error = errorEmail
+                    Toast.makeText(this@MainActivity, errorEmail, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
